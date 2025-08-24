e86bef379dd189d255eb0c84772a75948a41f937
Rudolf Bakhman <rudik.login@gmail.com>
2025-08-24 15:36:03 +0300

    check connection

--- app/main.py
+++ app/main.py
@@ -1,15 +1,12 @@
 from fastapi import FastAPI, HTTPException
 from starlette import status
 from app.schema import UserMessage
-from redis import redis
+from app.redis_db import redis_db
 from app.config import settings
 
 
 app = FastAPI(title=settings.PROJECT_NAME,
               version=settings.PROJECT_VERSION)
-
-
-redis.redis_connect()
 
 
 @app.post("/dialog/{from_user}/send/{to_user}")
@@ -18,7 +15,7 @@ def send_message_to_user(from_user: int, to_user: int, user_message: UserMessage
     # user_id_from_token = dep.get_current_user(token)
 
     try:
-        if redis.redis_db_send_message_from_to(from_user, to_user, user_message.message):
+        if redis_db.redis_db_send_message_from_to(from_user, to_user, user_message.message):
             return {"Message send": "ok", "from": from_user, "to": to_user}
         else:
             return {"Message send": "false"}
@@ -36,7 +33,7 @@ def get_messages_from_to_user(from_user: int, to_user: int):
 
     try:
         print(f"Request from {from_user} to {to_user}")
-        result = redis.redis_db_get_user_messages(from_user, to_user)
+        result = redis_db.redis_db_get_user_messages(from_user, to_user)
         print(f"Result query to Redis: {result}", flush=True)
         if result:
             return result
@@ -53,7 +50,7 @@ def get_messages_from_to_user(from_user: int, to_user: int):
 def get_all_messages_from_user(user_id: int):
     try:
         result_query = []
-        for r in redis.redis_search_user_dialog(user_id):
+        for r in redis_db.redis_search_user_dialog(user_id):
             result_query.append(r)
         return result_query
     except BaseException:
