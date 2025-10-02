a7454f95790168355f154d5b13fbf7eb870db8bb
Rudolf Bakhman <rudik.login@gmail.com>
2025-10-02 12:45:16 +0300

    Check response unread messages

--- app/main.py
+++ app/main.py
@@ -15,7 +15,8 @@ app = FastAPI(title=settings.PROJECT_NAME,
 def send_message_to_user(from_user: int, to_user: int, user_message: UserMessage):
     # token: str = Depends(dep.get_token)
     # user_id_from_token = dep.get_current_user(token)
-    url = f"http://{settings.DIALOG_UNREAD_MESSAGES_HOST}:{settings.DIALOG_UNREAD_MESSAGES_PORT}/{settings.DIALOG_UNREAD_ADD_URL}/{from_user}/{to_user}"
+    url = (f"http://{settings.DIALOG_UNREAD_MESSAGES_HOST}:{settings.DIALOG_UNREAD_MESSAGES_PORT}"
+           f"{settings.DIALOG_UNREAD_ADD_URL}{from_user}/{to_user}")
 
     try:
         if redis_db.redis_db_send_message_from_to(from_user, to_user, user_message.message):
